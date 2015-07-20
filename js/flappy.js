684d58ff81850e64684552ab1f89d2582e0b3073
V3493 (Conference ID) <v3493@cam.ac.uk>
2015-07-20 12:27:38 +0100

    hjk

--- js/flappy.js
+++ js/flappy.js
@@ -13,7 +13,10 @@ var game = new Phaser.Game(790, 400, Phaser.AUTO, 'game', stateActions);
  * Loads all resources for the game and gives them names.
  */
 function preload() {
-
+    game.load.image("James_Bond", "../assets/jamesBond.gif");
+    game.load.image("Frog", "../assets/flappy_frog.png");
+    game.load.image("Background", "../assets/Background_xp.jpg");
+    game.load.audio("score", "../assets/point.ogg");
 
 }
 
@@ -22,7 +25,31 @@ function preload() {
  */
 function create() {
     // set the background colour of the scene
+
+    var background = game.add.image(0, 0, "Background");
+    background.width = 790;
+    background.height = 400;
+    game.stage.setBackgroundColor("#00FF00");
+    game.add.text(290,100,"Hi :)",
+        {font:"100px Arial",fill:"#3399FF"});
+    game.add.sprite(50,50,"Frog");
+    game.add.sprite(740,50,"Frog");
+    game.add.sprite(740,350,"James_Bond");
+    game.add.sprite(50,350,"James_Bond");
+
+        game.input
+            .onDown
+            .add(clickHandler);
+game.input
+    .keyboard.addkey(Phaser.Keyboard.SPACEBAR)
+    .onDown.add(spaceHandler);
 }
+function clickHandler(event) {
+
+
+   game.add.sprite(event.x, event.y, "Frog");
+}
+
 
 /*
  * This function updates the scene. It is called for every new frame.
@@ -30,3 +57,8 @@ function create() {
 function update() {
 
 }
+
+function spaceHandler(){
+    game.sound.play("score");
+}
+
